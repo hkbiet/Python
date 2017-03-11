c691efcc39d676d05df2a2f5376e6c1a470c8cb5
root <root@localhost.localdomain>
2017-03-11 18:00:12 +0530

    Adding file 1.py

---
+++ 1.py
@@ -0,0 +1,8 @@
+import os
+import time
+while True:
+	for i in ["|","/","-","\\"]:
+		print "%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s" %(i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i)
+		#print "\n"*100
+		time.sleep(0.4)
+		os.system('clear')
