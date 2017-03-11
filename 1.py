91cb8e836c8c8fed97aee42cbf1dcda4ed8ab16b
ASTITVA <astitva@localhost.localdomain>
2017-03-11 18:06:40 +0530

    LEAVE IT AS IS

--- 1.py
+++ 1.py
@@ -4,6 +4,6 @@ while True:
 	for i in ["|","/","-","\\"]:
 		print "%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s" %(i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i)
 		#print "\n"*100
-		print "TEST CODE GOES HERE \"NOT NEEDED\""
+		#print "LEAVE IT AS IS"
 		time.sleep(0.4)
 		os.system('clear')
