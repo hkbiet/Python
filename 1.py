e8594410b72672030aacd17d5a9463cbb2dd5c74
ASTITVA <astitva@localhost.localdomain>
2017-03-11 18:02:55 +0530

    Modified 1.py

--- 1.py
+++ 1.py
@@ -4,5 +4,6 @@ while True:
 	for i in ["|","/","-","\\"]:
 		print "%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s" %(i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i)
 		#print "\n"*100
+		print "TEST CODE GOES HERE"
 		time.sleep(0.4)
 		os.system('clear')
