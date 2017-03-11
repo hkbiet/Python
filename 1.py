fd4d157e606a7182d4b20712cff57417a1be0c7f
root <root@localhost.localdomain>
2017-03-11 18:04:38 +0530

    MODIFIED CHANGES AGAIN

--- 1.py
+++ 1.py
@@ -4,6 +4,6 @@ while True:
 	for i in ["|","/","-","\\"]:
 		print "%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s" %(i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i)
 		#print "\n"*100
-		print "TEST CODE GOES HERE"
+		print "TEST CODE GOES HERE \"NOT NEEDED\""
 		time.sleep(0.4)
 		os.system('clear')
